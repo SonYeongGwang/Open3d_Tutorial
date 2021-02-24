01d36a8069f5e679f4edc2c827a10b695b14a12e
son_desk <syoungk20@naver.com>
2021-02-24 10:45:15 +0900

    Visualize point cloud and some post processing

--- Visualize_point_cloud.py
+++ Visualize_point_cloud.py
@@ -1,3 +1,16 @@
-import numpy as numpy
+import numpy as np
 import open3d as o3d
 
+print("Load a ply point cloud, print it, and render it")
+pcd = o3d.io.read_point_cloud("./fragment.ply")
+print(pcd)
+print(np.asarray(pcd.points))
+o3d.visualization.draw_geometries([pcd])
+
+print("Downsample the point cloud with a voxel of 0.05")
+downpcd = pcd.voxel_down_sample(voxel_size=0.09)
+o3d.visualization.draw_geometries([downpcd])
+
+print("Recompute the normal of the downsampled point cloud")
+downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+o3d.visualization.draw_geometries([downpcd], point_show_normal=True)

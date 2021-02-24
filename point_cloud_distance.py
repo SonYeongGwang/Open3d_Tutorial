d0f7a7da4db758b71f6ad23f5ceb42329879fef9
son_desk <syoungk20@naver.com>
2021-02-24 18:01:35 +0900

    clustering using DBSCAN

--- point_cloud_distance.py
+++ point_cloud_distance.py
@@ -10,7 +10,27 @@ dists = pcd.compute_point_cloud_distance(chair)
 # source=pcd & target=chair
 # It computes (for each point in the source point cloud) the distance to the closest point in the target point cloud
 dists = np.asarray(dists)
-ind = np.where(dists > 0.20)[0]
+ind = np.where(dists > 0.01)[0]
 print(ind)
 pcd_without_chair = pcd.select_by_index(ind)
 o3d.visualization.draw_geometries([pcd_without_chair])
+
+'''
+bounding volumes
+'''
+# https://hoodymong.tistory.com/8
+# obb 와 aabb의 차이점
+aabb = chair.get_axis_aligned_bounding_box()
+aabb.color = (0, 0, 1)
+obb = chair.get_oriented_bounding_box()
+obb.color = (1, 0, 0)
+o3d.visualization.draw_geometries([chair, aabb, obb])
+
+'''
+Convex hull(using Quick hull)
+'''
+downchair = chair.voxel_down_sample(voxel_size=0.05)
+hull, _ = downchair.compute_convex_hull()
+hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
+hull_ls.paint_uniform_color((0, 0, 1))
+o3d.visualization.draw_geometries([hull_ls])

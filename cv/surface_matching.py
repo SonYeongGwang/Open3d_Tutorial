8cba820e0b2e741d62e74e81d64a3aaf9adeaf6c
son_desk <syoungk20@naver.com>
2021-03-05 21:27:00 +0900

    complete surface_matching

--- cv/surface_matching.py
+++ cv/surface_matching.py
@@ -45,8 +45,12 @@ class SurfaceMatching:
         print("complete matching")
         return self.results
 
-    def Visualize(self, results):
+    def Visualize(self, results, line=True, box=True):
+        flag = 0
         pose = results[0].pose
+
+        org_color = [0.7, 0.2, 0.0]
+        est_color = [1, 0.7, 0]
 
         model = o3d.io.read_point_cloud(self.ModelPath)
         scene = o3d.io.read_point_cloud(self.ScenePath)
@@ -54,19 +58,63 @@ class SurfaceMatching:
 
         model_estimate = copy.deepcopy(model)
 
-        model.paint_uniform_color([0, 1, 0.7])
-        model_estimate.paint_uniform_color([1, 0.7, 0])
+        model.paint_uniform_color(org_color)
+        model_estimate.paint_uniform_color(est_color)
 
         model_estimate.transform(pose)
         # print("Model")
         # o3d.visualization.draw_geometries([model, mesh])
         # print("Scene")
         # o3d.visualization.draw_geometries([scene, mesh])
-        o3d.visualization.draw_geometries([model, scene, model_estimate, mesh])
+
+        if line:
+            np.random.seed(7)
+            indx = np.random.randint(0, len(model.points), (20,))
+
+            sel_point_org = model.select_by_index(indx)
+            sel_point_org = np.asarray(sel_point_org.points)
+            sel_point_est = model_estimate.select_by_index(indx)
+            sel_point_est = np.asarray(sel_point_est.points)
+
+            points = np.concatenate((sel_point_org, sel_point_est), axis=0)
+
+            lines = np.asarray([[i, i + len(points)/2]for i in np.arange(len(points)/2)])
+            lines.astype('int32')
+            colors = [[1, 0, 0] for i in range(len(lines))]
+            line_set = o3d.geometry.LineSet(
+                points=o3d.utility.Vector3dVector(points),
+                lines=o3d.utility.Vector2iVector(lines),
+            )
+            line_set.colors = o3d.utility.Vector3dVector(colors)
+            flag = flag + 1
+
+        if box:
+            obb_org = model.get_oriented_bounding_box()
+            obb_org.color = org_color
+            obb_est = model_estimate.get_oriented_bounding_box()
+            obb_est.color = est_color
+
+            if flag:
+                o3d.visualization.draw_geometries(
+                    [model, model_estimate, line_set, obb_org, obb_est])
+            
+            else:
+                o3d.visualization.draw_geometries(
+                    [model, model_estimate, obb_org, obb_est])
+        
+        else:
+            if flag:
+                o3d.visualization.draw_geometries(
+                    [model, model_estimate, line_set])
+
+            else:
+                o3d.visualization.draw_geometries(
+                    [model, model_estimate])
+
 
 sp = SurfaceMatching('./parasaurolophus_6700.ply', './rs1_normals.ply', ModelNor=1, SceneNor=1)
 print(sp)
 sp.Train()
 results = sp.Match()
 print(results[0].pose)
-sp.Visualize(results)
+sp.Visualize(results, line=False, box=False)
